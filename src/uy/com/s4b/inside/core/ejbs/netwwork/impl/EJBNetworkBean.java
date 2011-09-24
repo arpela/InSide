9104ac394b5ef6ebfc43ae083df1c8b14ac4554f
Alfredo Rodriguez <arpela@gmail.com>
2011-09-24 13:12:23 -0300

    prueba del arbol

---
+++ src/uy/com/s4b/inside/core/ejbs/netwwork/impl/EJBNetworkBean.java
@@ -0,0 +1,67 @@
+package uy.com.s4b.inside.core.ejbs.netwwork.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.hibernate.Hibernate;
+import org.jboss.ejb3.annotation.LocalBinding;
+
+import uy.com.s4b.inside.core.ejbs.netwwork.EJBNetworkLocal;
+import uy.com.s4b.inside.core.ejbs.netwwork.NetworkService;
+import uy.com.s4b.inside.core.entity.Network;
+import uy.com.s4b.inside.core.exception.InSideException;
+
+/**
+ * Title: EJBNetwork.java <br>
+ * Description: <br>
+ * Fecha creación: 22/09/2011 <br>
+ * Copyright: S4B <br>
+ * Company: S4B - http://www.s4b.com.uy <br>
+ * @author Alfredo
+ *
+ */
+@Stateless
+@Local(EJBNetworkLocal.class)
+@LocalBinding(jndiBinding="inSide/EJBNetwork/local")
+public class EJBNetworkBean implements NetworkService {
+
+	@PersistenceContext()
+	EntityManager em;
+	
+	
+	/**
+	 * 
+	 */
+	public EJBNetworkBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/* (non-Javadoc)
+	 * @see uy.com.s4b.inside.core.ejbs.netwwork.NetworkService#listNetwork()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Network> listNetwork() throws InSideException {
+		Query query = em.createNamedQuery("findAll");
+		List<Network> resultado = query.getResultList();
+		
+		if ((resultado != null) && (resultado.size() > 0)){			
+//			Collections.sort(resultado, new Network.SortName());
+			for (Network network : resultado) {
+				Hibernate.initialize(network.getColSite());
+			}
+		}
+		
+		return resultado;
+	}
+	
+	
+	
+}
--- src/uy/com/s4b/inside/ui/bean/SurfLogin.java
+++ src/uy/com/s4b/inside/ui/bean/SurfLogin.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 
 import javax.ejb.EJB;
@@ -15,11 +16,14 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
 import uy.com.s4b.inside.core.common.CriptPassword;
 import uy.com.s4b.inside.core.ejbs.manager.EJBManagerUserLocal;
+import uy.com.s4b.inside.core.ejbs.netwwork.EJBNetworkLocal;
+import uy.com.s4b.inside.core.entity.Network;
 import uy.com.s4b.inside.core.entity.User;
 import uy.com.s4b.inside.core.exception.InSideException;
 import uy.com.s4b.inside.ui.bundle.BundleUtil;
@@ -40,7 +44,10 @@ public class SurfLogin implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	EJBManagerUserLocal mangerUser;
+	EJBManagerUserLocal ejbMangerUser;
+	
+	@EJB
+	EJBNetworkLocal ejbNetwork;
 	
 	@ManagedProperty(value="#{login}")
 	private LoginBakingBean loginBaking;
@@ -56,6 +63,7 @@ public class SurfLogin implements Serializable {
 	}
 	
 	
+	
 	public String accionLogin(){
 		// setear el id de session para que luego se escriba en el log.
 		
@@ -63,8 +71,11 @@ public class SurfLogin implements Serializable {
 		String retorno = null;
 		try {
 			String pass = new CriptPassword().getHashSH1(loginBaking.getUserBean().getPass());
-			User u = mangerUser.login(loginBaking.getUserBean().getNick(), pass);
+			User u = ejbMangerUser.login(loginBaking.getUserBean().getNick(), pass);
 			loginBaking.setUserBean(u);
+			List<Network> listNetwork = ejbNetwork.listNetwork();
+			HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+			session.setAttribute("listaNetwork", listNetwork);
 			retorno = "site/home";
 		} catch (InSideException ex) {
 			log.error(ex.getKeyMSGError());
@@ -77,6 +88,7 @@ public class SurfLogin implements Serializable {
 		return retorno;
 	}
 
+	
 	
 	public void accionLogout() {
 		try {
@@ -95,8 +107,10 @@ public class SurfLogin implements Serializable {
 		}
 	}
 	
+	
+	
 	/**
-	 * hace el cambio de idima
+	 * hace el cambio de idioma por ahora no utilizado...
 	 * @return
 	 */
 	public String changeLeguage(){
