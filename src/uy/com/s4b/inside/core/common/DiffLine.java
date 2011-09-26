0808cc5b7fc518096a020d47f8139e499c39491b
Alfredo Rodriguez <arpela@gmail.com>
2011-09-25 22:48:09 -0300

    Se agrega ejb para reporte de diferencias

--- src/uy/com/s4b/inside/core/common/DiffLine.java
+++ src/uy/com/s4b/inside/core/common/DiffLine.java
@@ -1,5 +1,15 @@
 package uy.com.s4b.inside.core.common;
 
+/**
+ * 
+ * Title: DiffLine.java <br>
+ * Description: <br>
+ * Fecha creación: 21/09/2011 <br>
+ * Copyright: S4B <br>
+ * Company: S4B - http://www.s4b.com.uy <br>
+ * @author Pablo
+ *
+ */
 public enum DiffLine {
 	NEW, CHANGE, DELETE, MOVE, ERROR
 }
